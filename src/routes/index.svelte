01e335571285333d9cea32591b3f459a8704e24a
Lawrence N. Masullo <lmasullo@gmail.com>
2022-07-02 22:03:09 -0500

    added logo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,19 @@
-<h1>Future home of 201 Technologies</h1>
 <!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+<script>
+</script>
+
+<div class="main">
+	<!-- <h1>Future home of 201 Technologies</h1> -->
+	<!-- 201 image -->
+	<img src="201-technology.png" alt="201" class="logo" width="500" />
+</div>
+
+<style>
+	.main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+</style>
